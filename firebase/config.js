be8d992dc125b6d7c483f6a6cab1740e6242d6e4
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-09-16 18:47:57 -0300

    Integrando o Firebase ao React

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSxIF0GrygC3Yd6IdY5OyA-UuzjsIAlrA",
+  authDomain: "miniblog-4a60b.firebaseapp.com",
+  projectId: "miniblog-4a60b",
+  storageBucket: "miniblog-4a60b.appspot.com",
+  messagingSenderId: "979625175291",
+  appId: "1:979625175291:web:bdcb605464d6064882a85e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
