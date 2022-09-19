83cffa9d6cd61eb7a65f274f1471bee34ef9dc33
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-09-18 22:01:32 -0300

    Links para usuario autenticado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import { NavLink } from "react-router-dom";
 
+import { useAuthentication } from "../hooks/useAuthentication";
+
+import { useAuthValue } from "../context/AuthContext";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+    const { user } = useAuthValue();
   return (
     <nav className={styles.navbar}>
         <NavLink className={styles.brand} to={"/"}>
@@ -14,7 +19,9 @@ const Navbar = () => {
                     Home
                 </NavLink>
             </li>
-            <li>
+                {!user && (
+                    <>
+                    <li>
                 <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>
                     Entrar
                 </NavLink>
@@ -24,6 +31,22 @@ const Navbar = () => {
                     Cadastrar
                 </NavLink>
             </li>
+                    </>
+                )}
+                {user && (
+                     <>
+                     <li>
+                 <NavLink to="/posts/create" className={({isActive}) => (isActive ? styles.active : "")}>
+                     Novo post
+                 </NavLink>
+             </li>
+             <li>
+                 <NavLink to="/dashboard" className={({isActive}) => (isActive ? styles.active : "")}>
+                     Dashboard
+                 </NavLink>
+             </li>
+                     </>
+                )}
             <li>
                 <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>
                     Sobre
